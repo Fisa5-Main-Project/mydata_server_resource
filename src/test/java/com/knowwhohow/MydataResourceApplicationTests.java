92fe25490b43b40169434be9e7e143ba79be1ead
조원희 <woni0517@naver.com>
2025-11-11 09:30:39 +0900

    [chore] 프로젝트 세팅

---
+++ src/test/java/com/knowwhohow/MydataResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowwhohow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydataResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
